ec0e1968a8377bb311dcb9f7795e627dd052bea1
夏苗程 <miaxia@deloitte.com.cn>
2021-12-05 13:01:54 +0800

    Initial commit

---
+++ src/main/java/com/xhh/chat/XhhChatApplication.java
@@ -0,0 +1,13 @@
+package com.xhh.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhhChatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(XhhChatApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/xhh/chat/XhhChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhh.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhhChatApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
